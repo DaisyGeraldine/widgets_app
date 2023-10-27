9d4b6e4cf0977a1c730a727fde316124d0ba4a8d
Daisy <chipanal@hotmail.com>
2023-10-27 15:32:42 -0500

    ExpansionTile and CheckboxTile

--- lib/presentation/screens/ui_controls/ui_controlls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controlls_screen.dart
@@ -40,6 +40,9 @@ class _UiControlsViewState extends State<_UiControlsView> {
 
   bool isDeveloper = true;
   Transportation selectedTransportation = Transportation.car;
+  bool wantsBreakfast = false;
+  bool wantsLunch = false;
+  bool wantsDinner = false;
 
   @override
   Widget build(BuildContext context) {
@@ -57,83 +60,122 @@ class _UiControlsViewState extends State<_UiControlsView> {
           },
         ),
 
-        RadioListTile(
-          title: const Text('Radio Car'),
-          subtitle: const Text('Radio list Car'),
-          value: Transportation.car,
-          groupValue: selectedTransportation,
+        ExpansionTile(
+          title: const Text('Expansion Tile'),
+          subtitle: const Text('Expansion Tile Subtitle'),
+          children: [
+            RadioListTile(
+              title: const Text('Radio Car'),
+              subtitle: const Text('Radio list Car'),
+              value: Transportation.car,
+              groupValue: selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  selectedTransportation = Transportation.car;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Radio Bike'),
+              subtitle: const Text('Radio List bike'),
+              value: Transportation.bike,
+              groupValue: selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  selectedTransportation = Transportation.bike;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Radio Motorcycle'),
+              subtitle: const Text('Radio List Motorcycle'),
+              value: Transportation.motorcycle,
+              groupValue: selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  selectedTransportation = Transportation.motorcycle;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Radio Boat'),
+              subtitle: const Text('Radio List Boat'),
+              value: Transportation.boat,
+              groupValue: selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  selectedTransportation = Transportation.boat;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Radio Plane'),
+              subtitle: const Text('Radio List Plane'),
+              value: Transportation.plane,
+              groupValue: selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  selectedTransportation = Transportation.plane;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Radio Helicopter'),
+              subtitle: const Text('Radio List Helicopter'),
+              value: Transportation.helicopter,
+              groupValue: selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  selectedTransportation = Transportation.helicopter;
+                });
+              },
+            ),
+            RadioListTile(
+              title: const Text('Radio Rocket'),
+              subtitle: const Text('Radio List Rocket'),
+              value: Transportation.rocket,
+              groupValue: selectedTransportation,
+              onChanged: (value) {
+                setState(() {
+                  selectedTransportation = Transportation.rocket;
+                });
+              },
+            ),
+          ],
+        ),
+
+        CheckboxListTile(
+          title: const Text('¿Quieres desayuno?'),
+          //subtitle: const Text('Checkbox List Tile Subtitle'),
+          value: wantsBreakfast,
           onChanged: (value) {
             setState(() {
-              selectedTransportation = Transportation.car;
+              wantsBreakfast = !wantsBreakfast;
             });
           },
         ),
-        RadioListTile(
-          title: const Text('Radio Bike'),
-          subtitle: const Text('Radio List bike'),
-          value: Transportation.bike,
-          groupValue: selectedTransportation,
+        CheckboxListTile(
+          title: const Text('¿Quieres almuerzo?'),
+          //subtitle: const Text('Checkbox List Tile Subtitle'),
+          value: wantsLunch,
           onChanged: (value) {
             setState(() {
-              selectedTransportation = Transportation.bike;
+              wantsLunch = !wantsLunch;
             });
           },
         ),
-        RadioListTile(
-          title: const Text('Radio Motorcycle'),
-          subtitle: const Text('Radio List Motorcycle'),
-          value: Transportation.motorcycle,
-          groupValue: selectedTransportation,
+        CheckboxListTile(
+          title: const Text('¿Quieres cena?'),
+          //subtitle: const Text('Checkbox List Tile Subtitle'),
+          value: wantsDinner,
           onChanged: (value) {
             setState(() {
-              selectedTransportation = Transportation.motorcycle;
+              wantsDinner = !wantsDinner;
             });
           },
         ),
-        RadioListTile(
-          title: const Text('Radio Boat'),
-          subtitle: const Text('Radio List Boat'),
-          value: Transportation.boat,
-          groupValue: selectedTransportation,
-          onChanged: (value) {
-            setState(() {
-              selectedTransportation = Transportation.boat;
-            });
-          },
-        ),
-        RadioListTile(
-          title: const Text('Radio Plane'),
-          subtitle: const Text('Radio List Plane'),
-          value: Transportation.plane,
-          groupValue: selectedTransportation,
-          onChanged: (value) {
-            setState(() {
-              selectedTransportation = Transportation.plane;
-            });
-          },
-        ),
-        RadioListTile(
-          title: const Text('Radio Helicopter'),
-          subtitle: const Text('Radio List Helicopter'),
-          value: Transportation.helicopter,
-          groupValue: selectedTransportation,
-          onChanged: (value) {
-            setState(() {
-              selectedTransportation = Transportation.helicopter;
-            });
-          },
-        ),
-        RadioListTile(
-          title: const Text('Radio Rocket'),
-          subtitle: const Text('Radio List Rocket'),
-          value: Transportation.rocket,
-          groupValue: selectedTransportation,
-          onChanged: (value) {
-            setState(() {
-              selectedTransportation = Transportation.rocket;
-            });
-          },
-        ),
+
+        
 
 
       ],
