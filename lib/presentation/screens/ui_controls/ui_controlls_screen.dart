de0ecd8b5c5ac6541a6929f01964e1aa7b7f25af
Daisy <chipanal@hotmail.com>
2023-10-27 15:22:33 -0500

    CheckBox, Radios, and orhers Tiles

--- lib/presentation/screens/ui_controls/ui_controlls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controlls_screen.dart
@@ -8,6 +8,135 @@ class UiControlsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ui Controls Screen'),
+      ),
+      body: _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView({
+    super.key,
+  });
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation {
+  car,
+  bike,
+  motorcycle,
+  boat,
+  plane,
+  helicopter,
+  rocket,
+}
+
+class _UiControlsViewState extends State<_UiControlsView> {
+
+  bool isDeveloper = true;
+  Transportation selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Switch List Tile'),
+          subtitle: const Text('Switch List Tile Subtitle'),
+          value: isDeveloper,
+          onChanged: (value) {
+            setState(() {
+              isDeveloper = !isDeveloper;
+            });
+          },
+        ),
+
+        RadioListTile(
+          title: const Text('Radio Car'),
+          subtitle: const Text('Radio list Car'),
+          value: Transportation.car,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.car;
+            });
+          },
+        ),
+        RadioListTile(
+          title: const Text('Radio Bike'),
+          subtitle: const Text('Radio List bike'),
+          value: Transportation.bike,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.bike;
+            });
+          },
+        ),
+        RadioListTile(
+          title: const Text('Radio Motorcycle'),
+          subtitle: const Text('Radio List Motorcycle'),
+          value: Transportation.motorcycle,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.motorcycle;
+            });
+          },
+        ),
+        RadioListTile(
+          title: const Text('Radio Boat'),
+          subtitle: const Text('Radio List Boat'),
+          value: Transportation.boat,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.boat;
+            });
+          },
+        ),
+        RadioListTile(
+          title: const Text('Radio Plane'),
+          subtitle: const Text('Radio List Plane'),
+          value: Transportation.plane,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.plane;
+            });
+          },
+        ),
+        RadioListTile(
+          title: const Text('Radio Helicopter'),
+          subtitle: const Text('Radio List Helicopter'),
+          value: Transportation.helicopter,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.helicopter;
+            });
+          },
+        ),
+        RadioListTile(
+          title: const Text('Radio Rocket'),
+          subtitle: const Text('Radio List Rocket'),
+          value: Transportation.rocket,
+          groupValue: selectedTransportation,
+          onChanged: (value) {
+            setState(() {
+              selectedTransportation = Transportation.rocket;
+            });
+          },
+        ),
+
+
+      ],
+    );
   }
 }
