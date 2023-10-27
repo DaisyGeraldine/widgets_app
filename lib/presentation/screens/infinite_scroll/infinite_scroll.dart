8f4bd43e67c572eda6498d6bed76705e674cd314
Daisy <chipanal@hotmail.com>
2023-10-27 03:33:06 -0500

    screen created and actually

---
+++ lib/presentation/screens/infinite_scroll/infinite_scroll.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScrollScreen extends StatelessWidget {
+
+  static String name = 'Infinite.screen';
+
+  const InfiniteScrollScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/snackbar/snack_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SnackBarScreen extends StatelessWidget {
+
+  static String name = 'Infinite.screen';
+
+  const SnackBarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
